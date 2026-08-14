a14ec54d02d4203eb2ca0538a7475f9e917012f2
Liibobobo <linhhihihuhu@gmail.com>
2026-08-14 23:51:05 +0800

    method 2, assign character to new string

--- cs50p/playback/playback.py
+++ cs50p/playback/playback.py
@@ -1,9 +1,33 @@
-def playback():
+# def playback():
+#     speech = input ("His speech:")
+#     # split first
+#     speech_sep = speech.split()
+#     print (speech_sep)
+#     # join again with "..."
+#     new_speech = '...'.join(speech_sep)
+#     print (new_speech)
+# playback()
+
+# method 2: assign lan luot for new sentence, use concatenate
+def playback_2():
     speech = input ("His speech:")
-    # split first
-    speech_sep = speech.split()
-    print (speech_sep)
-    # join again with "..."
-    new_speech = '...'.join(speech_sep)
+    new_speech = ""
+    for i in range(len(speech)):
+        if (speech[i] != " "):
+            new_speech += speech[i]
+        else:
+            new_speech += "..."
     print (new_speech)
-playback()
+
+
+playback_2()
+
+    # j = 0
+    # for i in range(len(speech)):
+    #     if (speech[i] != " "):
+    #         new_speech[j] = speech[i]
+    #         j += 1
+    #     else:
+    #         new_speech[j:j+2] = "."
+    #         j += 3
+    # print (new_speech)
