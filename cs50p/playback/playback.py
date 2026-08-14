ac4a6b030f3d8204933d349cdf2f76d26cc163f7
Liibobobo <linhhihihuhu@gmail.com>
2026-08-14 02:11:41 +0000

    Fri, Aug 14, 2026, 10:11 AM +08:00

--- cs50p/playback/playback.py
+++ cs50p/playback/playback.py
@@ -4,7 +4,7 @@ def playback():
     #replace space with \t
     new_speech = speech.replace(" ","\t")
    # new_speech = speech.expandtabs()
-    print (new_speech)
+    print (new_speech.expandtabs(3))
 playback()
 
 
