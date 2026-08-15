73634091130049ed1e5a4c3e7d91b2b53e5cc934
Liibobobo <linhhihihuhu@gmail.com>
2026-08-15 23:24:57 +0800

    method 1: copy from original string to new string, correct

--- cs50p/faces/faces.py
+++ cs50p/faces/faces.py
@@ -1,20 +1,24 @@
 def convert():
     typing = input("Typing:") 
     result = ""
-    for i in range(len(typing)):
+    i = 0
+    while i < len(typing):
         if (typing[i:i+2] == ":)"):
             result += "🙂"
-            i = i + 1
+            i = i + 2
             # result += typing [i]
             print (result)
         elif (typing[i:i+2] == ":("): 
             result += "🙁" 
-            # i = i + 1
+            i = i + 2
             # result += typing [i]
         else: 
             result += typing [i]
+            i = i + 1
             print (result)
         print (result)
         
 convert() 
+
+
         
