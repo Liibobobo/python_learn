4926d034021b1f8b20c90c964a2ac7a9963e85b3
Liibobobo <linhhihihuhu@gmail.com>
2026-08-16 01:21:09 +0800

    using available python funtion

--- cs50p/faces/faces.py
+++ cs50p/faces/faces.py
@@ -1,24 +1,38 @@
+# def main(): 
+#     typing = input("Typing:") 
+#     print (convert(typing))
+
+# def convert(typing):
+#     # typing = input("Typing:") 
+#     result = ""
+#     i = 0
+#     while i < len(typing):
+#         if (typing[i:i+2] == ":)"):
+#             result += "🙂"
+#             i = i + 2
+#         elif (typing[i:i+2] == ":("): 
+#             result += "🙁" 
+#             i = i + 2
+#         else: 
+#             result += typing [i]
+#             i = i + 1
+#     return result     
+
+# main ()
+
+#method 2, function from python
 def main(): 
-    typing = input("Typing:") 
-    print (convert(typing))
+    message = input("Typing:") 
+    print (convert(message))
 
 def convert(typing):
-    # typing = input("Typing:") 
-    result = ""
-    i = 0
-    while i < len(typing):
-        if (typing[i:i+2] == ":)"):
-            result += "🙂"
-            i = i + 2
-        elif (typing[i:i+2] == ":("): 
-            result += "🙁" 
-            i = i + 2
-        else: 
-            result += typing [i]
-            i = i + 1
-    return result     
 
+    result = ""
+    result = typing.replace(":)","🙂")
+    result = result.replace(":(","🙁")
+    return result
 main ()
+
 
 
         
