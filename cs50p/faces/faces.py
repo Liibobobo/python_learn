08af4a7d02b15e4983cd2cbd14dfa7c02fa12c52
Liibobobo <linhhihihuhu@gmail.com>
2026-08-16 00:44:13 +0800

    use funtion main to call function convert

--- cs50p/faces/faces.py
+++ cs50p/faces/faces.py
@@ -1,24 +1,24 @@
-def convert():
+def main(): 
     typing = input("Typing:") 
+    print (convert(typing))
+
+def convert(typing):
+    # typing = input("Typing:") 
     result = ""
     i = 0
     while i < len(typing):
         if (typing[i:i+2] == ":)"):
             result += "🙂"
             i = i + 2
-            # result += typing [i]
-            print (result)
         elif (typing[i:i+2] == ":("): 
             result += "🙁" 
             i = i + 2
-            # result += typing [i]
         else: 
             result += typing [i]
             i = i + 1
-            print (result)
-        print (result)
-        
-convert() 
+    return result     
+
+main ()
 
 
         
