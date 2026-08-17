7af71fd5a9aab6be32a5994f3a1519d6d67a780e
Liibobobo <linhhihihuhu@gmail.com>
2026-08-17 23:10:26 +0800

    playback ex with 2 methods

--- cs50p/playback/playback.py
+++ cs50p/playback/playback.py
@@ -1,18 +1,33 @@
-# python method
 # def playback():
 #     speech = input ("His speech:")
-#     #replace space with \t
-#     new_speech = speech.replace(" ","\t")
-#    # new_speech = speech.expandtabs()
-#     print (new_speech.expandtabs(3))
+#     # split first
+#     speech_sep = speech.split()
+#     print (speech_sep)
+#     # join again with "..."
+#     new_speech = '...'.join(speech_sep)
+#     print (new_speech)
 # playback()
 
-# again
-def playback():
+# method 2: assign lan luot for new sentence, use concatenate
+def playback_2():
     speech = input ("His speech:")
-    # split first
-    speech_sep = speech.split()
-    print (speech_sep)
-    speech_sep = speech_sep.append("...")
-    print (speech_sep)
-playback()
+    new_speech = ""
+    for i in range(len(speech)):
+        if (speech[i] != " "):
+            new_speech += speech[i]
+        else:
+            new_speech += "..."
+    print (new_speech)
+
+
+playback_2()
+
+    # j = 0
+    # for i in range(len(speech)):
+    #     if (speech[i] != " "):
+    #         new_speech[j] = speech[i]
+    #         j += 1
+    #     else:
+    #         new_speech[j:j+2] = "."
+    #         j += 3
+    # print (new_speech)
