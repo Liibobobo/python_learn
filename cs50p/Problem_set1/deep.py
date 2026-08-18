880cad3b75aa1cd6ab741777115c060e916e1597
Liibobobo <linhhihihuhu@gmail.com>
2026-08-19 00:12:24 +0800

    final commit with resolved corner case for 42

--- cs50p/Problem_set1/deep.py
+++ cs50p/Problem_set1/deep.py
@@ -1,5 +1,11 @@
 def main(): 
-    question = input("Great Question of Life, the Universe and Everything?")
+    # separate input to resolve corner case of 42 (space around 42)"
+    question_sep = input("Great Question of Life, the Universe and Everything?").split()
+    print (question_sep)
+    question = " ".join(question_sep)
+    print (question)
+
+    # resolve case-insensitively for 2 left input, lower all the input 
     question_lower = question.lower()
     if (question_lower == "42" or question_lower == "forty-two" or question_lower == "forty two"):
         print("Yes")
