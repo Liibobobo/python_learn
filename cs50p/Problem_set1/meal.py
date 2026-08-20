cb6c3880e293650bc9859a3977d61f681746d330
Liibobobo <linhhihihuhu@gmail.com>
2026-08-20 18:59:04 +0800

    commit for 'meal' ex with only 24 hour time - correct

--- cs50p/Problem_set1/meal.py
+++ cs50p/Problem_set1/meal.py
@@ -1,11 +1,18 @@
 def main():
-    time = input("What's time is it? ").split(":")
+    time = input("What's time is it? ")
     # breakfast 7:00 - 8:00
-    print(convert(time))
+    if 7 <= convert(time) <= 8 :
+        print ("breakfast time")
+    elif 12 <= convert(time) <= 13:
+        print ("lunch time")
+    elif 18 <= convert (time) <= 19:
+        print ("dinner time")
 
 def convert(time):
-    hour = int(time[0]) + int(time[1])/60 
-    return hour
+
+    hour, minute = time.split(":")
+    time_convert = int(hour) + int(minute)/60 
+    return time_convert 
 
 if __name__ == "__main__":
     main()
