535dc6d4763455afd7d497a4684944811482c410
Liibobobo <linhhihihuhu@gmail.com>
2026-09-24 02:29:39 +0800

    update what I learnt

--- cs50p/Problem_set1/meal.py
+++ cs50p/Problem_set1/meal.py
@@ -1,5 +1,6 @@
 def main():
     time = input("What's time is it? ")
+
     # breakfast 7:00 - 8:00
     if 7 <= convert(time) <= 8 :
         print ("breakfast time")
@@ -11,7 +12,15 @@ def main():
 def convert(time):
 
     hour, minute = time.split(":")
-    time_convert = int(hour) + int(minute)/60 
+
+    # my idea is met pm, the final time convert shoud + 12 hour ; other must am or not, just keep convert normal
+    if "p.m." in minute: 
+        minute = minute.removesuffix("p.m.")
+        time_convert = 12 + int(hour) + int(minute)/60 
+    else: 
+        minute = minute.removesuffix("a.m.")
+        time_convert = int(hour) + int(minute)/60 
+
     return time_convert 
 
 if __name__ == "__main__":
